3b9cbf6ab3ccf49f5321550d07d6efe576112e85
enochwu <enochwu7@gmail.com>
2017-12-21 14:17:24 -0800

    Element of Time
    Time calc added. However current time is showing as +/-12 hoursand that is not accurate.

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@ var database = firebase.database();
       var firstTrainInput = $("#first-train").val().trim();
       var freqInput = $("#frequency").val().trim();
 
-      if (nameInput && destInput && firstTrainTime && freqInput) {
+      if (nameInput && destInput && firstTrainInput && freqInput) {
 
         database.ref().push({
           train: nameInput,
@@ -21,8 +21,40 @@ var database = firebase.database();
         database.ref().on("child_added", function(childSnapshot) {
           console.log(childSnapshot.val().train);
           console.log(childSnapshot.val().destination);
+
+          var tFrequency = childSnapshot.val().trainFrequency;
+
+          // Time is 3:30 AM
+          var firstTime = childSnapshot.val().firstTrainTime;
+
+          // First Time (pushed back 1 year to make sure it comes before current time)
+          var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+          console.log(firstTimeConverted);
+
+          // Current Time
+          var currentTime = moment();
+          console.log(currentTime);
+          console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+          // Difference between the times
+          var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+          console.log("DIFFERENCE IN TIME: " + diffTime);
+
+          // Time apart (remainder)
+          var tRemainder = diffTime % tFrequency;
+          console.log(tRemainder);
+
+          // Minute Until Train
+          var tMinutesTillTrain = tFrequency - tRemainder;
+          console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+          // Next Train
+          var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+          var timeNextTrain = moment(nextTrain).format("hh:mm")
+          console.log("ARRIVAL TIME: " + timeNextTrain);
+
           var tableRow = $('<tr />')
-          var columnElement = tableRow.html('<td>'+childSnapshot.val().train+'</td><td>'+childSnapshot.val().destination+'</td><td>'+childSnapshot.val().firstTrainTime+'</td><td>'+childSnapshot.val().trainFrequency+'</td>')
+          var columnElement = tableRow.html('<td>'+childSnapshot.val().train+'</td><td>'+childSnapshot.val().destination+'</td><td>'+tMinutesTillTrain+'m'+'</td><td>'+timeNextTrain+'</td><td>'+childSnapshot.val().trainFrequency+'m'+'</td>')
 
           $("#train-schedule").append(tableRow);
 
