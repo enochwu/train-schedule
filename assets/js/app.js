6fbb982f1729b06eadcb8d8523fbb4ac0acb8f35
enochwu <enochwu7@gmail.com>
2017-12-22 00:14:39 -0800

    Last minute rework
    child added block cannot be within on-click block, or else data is not persistant and table is empty on refresh until new values are added to database. This is fixed.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,15 @@
+var config = {
+  apiKey: "AIzaSyAnk75zDMZ9armX5_xHaMy4vYx51k9WEow",
+  authDomain: "rain-scheduler.firebaseapp.com",
+  databaseURL: "https://rain-scheduler.firebaseio.com",
+  projectId: "rain-scheduler",
+  storageBucket: "",
+  messagingSenderId: "114010683972"
+};
+
+firebase.initializeApp(config);
 
 var database = firebase.database();
-
-console.log(database.destination);
-
-// var tableRow = $('<tr />')
-// var columnElement = tableRow.html('<td>'+database.train+'</td><td>'+childSnapshot.val().destination+'</td><td>'+tMinutesTillTrain+'m'+'</td><td>'+timeNextTrain+'</td><td>'+childSnapshot.val().trainFrequency+'m'+'</td>')
-// $("#train-schedule").append(tableRow);
 
 $("#add-train").on("click", function() {
   event.preventDefault();
@@ -17,59 +21,66 @@ $("#add-train").on("click", function() {
 
   if (nameInput && destInput && firstTrainInput && freqInput) {
 
-    database.ref().push({
+    // Creates local "temporary" object for holding employee data
+    var trainInfo = {
       train: nameInput,
       destination: destInput,
       firstTrainTime: firstTrainInput,
       trainFrequency: freqInput
-    });
+    };
 
-    database.ref().on("child_added", function(childSnapshot) {
-      console.log(childSnapshot.val().train);
-      console.log(childSnapshot.val().destination);
-
-      var tFrequency = childSnapshot.val().trainFrequency;
-
-      // Time is 3:30 AM
-      var firstTime = childSnapshot.val().firstTrainTime;
-
-      // First Time (pushed back 1 year to make sure it comes before current time)
-      var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-      console.log(firstTimeConverted);
-
-      // Current Time
-      var currentTime = moment();
-      console.log(currentTime);
-      console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
-
-      // Difference between the times
-      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-      console.log("DIFFERENCE IN TIME: " + diffTime);
-
-      // Time apart (remainder)
-      var tRemainder = diffTime % tFrequency;
-      console.log(tRemainder);
-
-      // Minute Until Train
-      var tMinutesTillTrain = tFrequency - tRemainder;
-      console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-      // Next Train
-      var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-      var timeNextTrain = moment(nextTrain).format("HH:mm")
-      console.log("ARRIVAL TIME: " + timeNextTrain);
-
-      var tableRow = $('<tr />')
-      var columnElement = tableRow.html('<td>'+childSnapshot.val().train+'</td><td>'+childSnapshot.val().destination+'</td><td>'+tMinutesTillTrain+'m'+'</td><td>'+timeNextTrain+'</td><td>'+childSnapshot.val().trainFrequency+'m'+'</td>')
-
-      $("#train-schedule").append(tableRow);
-
-    });
+    // Uploads employee data to the database
+    database.ref().push(trainInfo);
 
     $('.form-control').val('');
 
   } else {
     alert('Please fill out all the fields before submitting.')
   }
+
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+
+  var nameInput = childSnapshot.val().train;
+  var destInput = childSnapshot.val().destination;
+  var freqInput = childSnapshot.val().trainFrequency;
+
+  var tFrequency = childSnapshot.val().trainFrequency;
+
+  // Time is 3:30 AM
+  var firstTime = childSnapshot.val().firstTrainTime;
+  console.log(nameInput);
+
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+  // console.log(firstTimeConverted);
+
+  // Current Time
+  var currentTime = moment();
+  // console.log(currentTime);
+  // console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  // console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % tFrequency;
+  // console.log(tRemainder);
+
+  // Minute Until Train
+  var tMinutesTillTrain = tFrequency - tRemainder;
+  // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  var timeNextTrain = moment(nextTrain).format("HH:mm")
+  // console.log("ARRIVAL TIME: " + timeNextTrain);
+
+  var tableRow = $('<tr />')
+  var columnElement = tableRow.html('<td>'+nameInput+'</td><td>'+destInput+'</td><td>'+tMinutesTillTrain+'m'+'</td><td>'+timeNextTrain+'</td><td>'+freqInput+'m'+'</td>')
+
+  $("#train-schedule").append(tableRow);
 
 });
